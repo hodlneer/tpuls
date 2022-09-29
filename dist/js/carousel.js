a3d90382e26f7cabce281c241ac466f191c7271a
umerinayat <umer@ugcmedia.com>
2022-09-29 07:56:47 -0700

    carosuel updated

--- dist/js/carousel.js
+++ dist/js/carousel.js
@@ -1,25 +1,54 @@
 
 const cLayers = document.querySelectorAll('.layer');
 
-const cImages = document.querySelectorAll('.carousel-img');
+const cImages = document.querySelectorAll('.carousel-img'); //
+
+let current = cImages[0];
+
+function handleMediaQuery(x) {
+    if (x.matches) { // If media query matches
+       if(current) {
+        // $(current).append($('#' +  $(current).find(':first-child').data('id'))[0])
+       }
+    } else {
+        $('#img-content').append($('#' +  $(current).find(':first-child').data('id'))[0]);
+    }
+  }
+
+let x = window.matchMedia("(max-width: 929px)")
+handleMediaQuery(x) 
+x.addEventListener('change', () => {
+    handleMediaQuery(x);
+});
+
 
 
 cLayers.forEach(function(layer) {
     
     layer.addEventListener('click', function(event) {
 
-        let img = $(this).parent()[0];
+        let img = $(this).parent()[0]; //
         
 
         if( img.classList.contains('not-active') ) {
 
             cImages.forEach(function(oimg) {
+                
                 oimg.classList.remove('active');
                 oimg.classList.add('not-active');
+
+                current = oimg;
+
+                if (!x.matches) { 
+                    $('#img-content').append($('#' +  $(oimg).find(':first-child').data('id'))[0]);
+                } else {
+                    $(oimg).append($('#' +  $(oimg).find(':first-child').data('id'))[0])
+                }
                 
-                $(oimg).find(':first-child').fadeIn('slow');
-                $('#' +  $(oimg).find(':first-child').data('id')).hide();
-                $('#' +  $(oimg).find(':first-child').data('content')).hide();
+                $(oimg).find(':first-child').fadeIn('slow')//
+                $('#' +  $(oimg).find(':first-child').data('id')).hide()//
+                $('#' +  $(oimg).find(':first-child').data('content')).hide()//
+
             });
 
             img.classList.remove('not-active');
